5b535698985f9a5ecb6fa21445df88c7d20f46f2
DESKTOP-9J7QVV9\力诺科技 <405232275@qq.com>
2019-03-22 16:56:56 +0800

    5425424

--- vueac/src/components/MyFooter.vue
+++ vueac/src/components/MyFooter.vue
@@ -1,10 +1,13 @@
 <template>
-    $END$
+  <section>
+    <h1>崔永康巨TM帅</h1>
+    <h2>这是MyFooter组件</h2>
+  </section>
 </template>
 
 <script>
     export default {
-        name: "Myfooter"
+        name: "MyFooter"
     }
 </script>
 
