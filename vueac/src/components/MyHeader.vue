9cc6c2cdf2f608b50c4c5fe50c41ce5acf125041
SC-201811011602\lanou <1294923276@qq.com>
2019-03-22 16:50:12 +0800

    jdsajh

---
+++ vueac/src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+      <h2>这是奥特曼打小怪兽头部</h2>
+      <h2>齐书准是个大帅哥</h2>
+      <h2>何世英是个大帅哥</h2>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "MyHeadwe"
+    }
+</script>
+
+<style scoped>
+
+</style>
